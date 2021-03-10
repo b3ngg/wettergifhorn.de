0db96143747f50a5d5fd4d7cd2eec5cfa0b22563
Ben Weber <ben@wettergifhorn.de>
2021-03-10 18:11:03 +0100

    Add fetching for optional post data and content

--- src/api/notion.ts
+++ src/api/notion.ts
@@ -1,5 +1,5 @@
 import { get, stringToDate } from './helper';
-import type { CollectionResponse, PageResponse, Post } from './types';
+import type { Block, CollectionResponse, PageResponse, Post } from './types';
 
 const ENDPOINT = 'https://notion-api.splitbee.io/v1/';
 
@@ -41,4 +41,53 @@ export const getCollectionAsPosts = async (id: string): Promise<Post[]> => {
       };
     }
   );
+};
+
+/**
+ * Extract text blocks from a given page
+ * @param page The page object
+ * @param pageData The first page block containing meta data
+ * @returns Array of blocks
+ */
+export const getTextBlocksFromPage = (
+  page: PageResponse,
+  pageData: Record<string, any>
+): Block[] => {
+  const content = pageData.content as string[];
+
+  return content
+    .map((contentId) => {
+      const block = page[contentId].value as Record<string, any>;
+      if (block.type !== 'text') return;
+
+      const value = block.properties?.title
+        ?.map((value) => value[0])
+        .join('') as string;
+
+      return {
+        type: 'text',
+        content: value ?? '',
+      };
+    })
+    .filter((block) => block.content !== '');
+};
+
+/**
+ * Fetch aditional and optional data for a page
+ * It's useful to have a different function for data that needs a individual request,
+ * so the function can be called after the initial page load for post that are visible.
+ * @param post Post object with required data
+ * @returns Post object with added optional data, if presen (otherwise the value is undefined)
+ */
+export const addAdditonalDataToPost = async (post: Post): Promise<Post> => {
+  const page = await getPage(post.id);
+  // First page block containing page data
+  const pageData = Object.values(page)[0].value as Record<string, any>;
+
+  return {
+    ...post,
+    icon: pageData.format?.page_icon,
+    image: pageData.format?.page_cover,
+    blocks: getTextBlocksFromPage(page, pageData),
+  };
 };
