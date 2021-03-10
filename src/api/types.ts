05b1c31458d05dc6e93feb74341b00d27a580ed3
Ben Weber <ben@wettergifhorn.de>
2021-03-10 13:58:27 +0100

    Add interface for post

--- src/api/types.ts
+++ src/api/types.ts
@@ -1,2 +1,16 @@
 export type CollectionResponse = Record<string, string>[];
 export type PageResponse = Record<string, Record<string, unknown>>;
+
+export interface Post {
+  title: string;
+  date: Date;
+  tags: string[];
+  icon: string;
+  image: string;
+  blocks: Block[];
+}
+
+export interface Block {
+  type: string;
+  content: string;
+}
