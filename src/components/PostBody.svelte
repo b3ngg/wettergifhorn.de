4bf66fd28ca5f4644baba22f98206fdc766af2f8
Ben Weber <ben@wettergifhorn.de>
2021-03-11 07:50:16 +0100

    Add styling to post body

--- src/components/PostBody.svelte
+++ src/components/PostBody.svelte
@@ -4,8 +4,19 @@
   export let blocks: Block[];
 </script>
 
-<div class="body">
+<div class="container">
   {#each blocks as block}
     <p>{block.content}</p>
   {/each}
 </div>
+
+<style>
+  .container {
+    padding: 0 2rem;
+  }
+
+  p {
+    margin: 0;
+    padding: 0.2rem 0;
+  }
+</style>
