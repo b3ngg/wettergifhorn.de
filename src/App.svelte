dbe29b882be3230ce1d728efc35a841051fc8434
Ben Weber <ben@wettergifhorn.de>
2021-03-11 18:17:57 +0100

    Add info component to start page

--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,15 @@
 
   import Header from './components/Header.svelte';
   import ImageGenerator from './components/ImageGenerator.svelte';
+  import Info from './components/Info.svelte';
   import Post from './components/Post.svelte';
   import Spinner from './components/Spinner.svelte';
 
   let posts = getCollectionAsPosts('d85f75983c61439a87f132323a409d0d');
 
   let id = new URL(window.location.href).searchParams.get('id');
+
+  let showInfo = false;
 </script>
 
 {#if id}
@@ -17,15 +20,19 @@
   <div class="wrapper">
     <Header />
 
-    {#await posts}
-      <Spinner />
-    {:then value}
-      {#each value as item}
-        <Post data={item} />
-      {/each}
-    {:catch error}
-      Fehler: {error}
-    {/await}
+    {#if showInfo}
+      <Info />
+    {:else}
+      {#await posts}
+        <Spinner />
+      {:then value}
+        {#each value as item}
+          <Post data={item} />
+        {/each}
+      {:catch error}
+        Fehler: {error}
+      {/await}
+    {/if}
   </div>
 {/if}
 
