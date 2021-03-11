eb842c162d046d0053824682a258c4027d51095c
Ben Weber <ben@wettergifhorn.de>
2021-03-11 07:09:04 +0100

    Add asynchronous loading of posts

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,23 @@
 <script>
+  import { getCollectionAsPosts } from './api/notion';
+
   import Header from './components/Header.svelte';
+
+  let posts = getCollectionAsPosts('d85f75983c61439a87f132323a409d0d');
 </script>
 
 <div class="wrapper">
   <Header />
+
+  {#await posts}
+    Lade Posts
+  {:then value}
+    {#each value as item}
+      {item.title}
+    {/each}
+  {:catch error}
+    Fehler: {error}
+  {/await}
 </div>
 
 <style>
