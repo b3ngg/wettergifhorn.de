a8dcffe64d9a3a4cc79876c8d0d66e24e7c3ee3b
Ben Weber <ben@wettergifhorn.de>
2021-03-11 12:49:40 +0100

    Add custom colors to tags

--- src/components/Tag.svelte
+++ src/components/Tag.svelte
@@ -1,8 +1,32 @@
 <script lang="ts">
+  const categories = [
+    { keywords: ['sonnig'], h: 44, s: 94, l: 58 },
+    { keywords: ['gefährlich', 'gefahr'], h: 5, s: 60, l: 51 },
+  ];
+
+  const colors = (value: string): [string, string] => {
+    for (const category of categories) {
+      for (const keyword of category.keywords) {
+        if (value.includes(keyword)) {
+          return [
+            `hsl(${category.h}, ${category.s}%, ${category.l}%)`,
+            `hsl(${category.h}, ${category.s}%, 94%)`,
+          ];
+        }
+      }
+    }
+
+    return ['var(--c-blue)', 'var(--c-soft-blue)'];
+  };
+
   export let value: string;
+
+  console.log(colors(value));
 </script>
 
-<div>{value}</div>
+<div style="color: {colors(value)[0]}; background-color: {colors(value)[1]};">
+  {value}
+</div>
 
 <style>
   div {
