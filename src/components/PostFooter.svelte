ba9982cbe31fb299902ae2908a4146dcc9f77972
Ben Weber <ben@wettergifhorn.de>
2021-03-11 10:17:36 +0100

    Add styling to post footer

--- src/components/PostFooter.svelte
+++ src/components/PostFooter.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
+  import dayjs from 'dayjs';
+  import relativeTime from 'dayjs/plugin/relativeTime';
+  import 'dayjs/locale/de';
+
+  dayjs.locale('de');
+  dayjs.extend(relativeTime);
+
   export let date: Date;
 </script>
 
-<div class="footer">
-  {date}
+<div class="container">
+  {dayjs(date).fromNow()}
 </div>
+
+<style>
+  .container {
+    padding: 2rem;
+    color: var(--c-grey);
+    font-size: 0.8rem;
+  }
+</style>
