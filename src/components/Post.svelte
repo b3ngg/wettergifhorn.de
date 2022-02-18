e9919abae897ace2d694c0d6a193465a69e50b22
Ben Weber <ben@wettergifhorn.de>
2022-02-18 11:10:38 +0100

    Add support for posts with images

--- src/components/Post.svelte
+++ src/components/Post.svelte
@@ -7,7 +7,7 @@
 	export let post: Post;
 </script>
 
-<Box>
+<Box image={post.image}>
 	<div class="items-center pb-4 md:flex md:space-x-8">
 		<div class="pb-4 text-7xl md:pb-0 md:text-8xl">{post.icon ?? '💬'}</div>
 		<div>
