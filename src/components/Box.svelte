5192de6e8266e308aa7dcb5ea9d97837a5d9f385
Ben Weber <ben@wettergifhorn.de>
2022-02-18 10:58:42 +0100

    Improve box design

--- src/components/Box.svelte
+++ src/components/Box.svelte
@@ -1,3 +1,3 @@
-<div class="mb-12 max-w-4xl rounded-xl bg-white p-8 shadow-lg md:p-10">
+<div class="mb-12 max-w-4xl rounded-xl bg-white/80 bg-gradient-to-b  p-8 shadow-lg md:p-10">
 	<slot />
 </div>
