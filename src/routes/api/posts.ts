9d787478e5a0e68ce47dd988a9cb989f2f723516
b3ngg <ben@b3n.gg>
2022-08-18 08:23:30 +0200

    Update notion id

--- src/routes/api/posts.ts
+++ src/routes/api/posts.ts
@@ -5,7 +5,7 @@ export const get: RequestHandler = async ({ url }) => {
 	const start = +url.searchParams.get('start') || 0;
 	const end = +url.searchParams.get('end') || 4;
 
-	const allEntries = await getCollectionAsPosts('d85f75983c61439a87f132323a409d0d');
+	const allEntries = await getCollectionAsPosts('e97b96a82dd64cfeac1ae43f4314e194');
 	const requestedEntries = allEntries.slice(start, end);
 
 	const posts = await Promise.all(requestedEntries.map((entry) => addAdditionalDataToPost(entry)));
